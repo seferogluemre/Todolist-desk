fe82f836c452f9f5503c977ea39a4876e7d5b346
seferogluemre <yunusemre5331@icloud.com>
2025-02-02 22:02:34 +0300

    clear text areas

--- src/program.py
+++ src/program.py
@@ -4,22 +4,27 @@ def show_text():
     entered_text = entry_field.get()
     label.config(text=entered_text)
 
-# Create main window
+# main window
 window = tk.Tk()
 window.title("Tkinter GUI")
 window.geometry("300x200")
 
-# Entry field
 entry_field = tk.Entry(window, width=25)
 entry_field.pack(pady=10)
 
-# Button
 button = tk.Button(window, text="Show", command=show_text)
 button.pack()
-
-# Label
 label = tk.Label(window, text="Text will appear here", font=("Arial", 12))
 label.pack(pady=10)
 
-# Run the window
+def clear_text():
+    entry_field.delete(0, tk.END)  # Entry alanını temizle
+    label.config(text="Text will appear here")  # Etiketi sıfırla
+
+# Clear Button
+clear_button = tk.Button(window, text="Clear", command=clear_text)
+clear_button.pack(pady=5)
+
+
+
 window.mainloop()
