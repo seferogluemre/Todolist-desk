2a500de796a1d972d976d640e7ca5b4c350ade64
seferogluemre <yunusemre5331@icloud.com>
2025-02-02 21:57:32 +0300

    created interface

--- src/program.py
+++ src/program.py
@@ -4,3 +4,16 @@ def display_text():
     enter_text=.get()
     ticket.config(text=enter_text)
 
+window=tk.Tk();
+window.title("Interface")
+window.geometry("300x200")
+
+# Buton
+buton = tk.Button(window, text="Göster", command=display_text)
+buton.pack()
+
+# Etiket
+tag = tk.Label(window, text="Burada gözükecek", font=("Arial", 12))
+tag.pack(pady=10)
+
+window.mainloop()
